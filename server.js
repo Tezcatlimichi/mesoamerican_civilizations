b9159fe38d1134eabeeef4ca788b2ca303a77df2
Kathya Castaneda <kathyakathya.kc@gmail.com>
2022-10-24 10:49:27 -0500

    created a get route for civilizations

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const PORT = process.env.PORT || 3001
 const db = require('./db')
-const { Country } = require('./models')
+const { Country, Civilization } = require('./models')
 
 const app = express()
 //middleware
@@ -10,8 +10,7 @@ app.use(express.json())
 app.get('/', (req, res) => {
   res.send({ msg: 'This route is being hit' })
 })
-
-//Country route
+//Countries
 // create country route
 app.post('/countries', async (req, res) => {
   let createdCountry = await Country.create(req.body)
@@ -37,7 +36,12 @@ app.delete('/countries/:id', async (req, res) => {
 
   res.json(deletedCountry)
 })
-
+//Civilizations
+//create civilization
+app.get('/civilizations', async (req, res) => {
+  const allCivilizations = await Civilization.find({})
+  res.json(allCivilizations)
+})
 app.listen(PORT, () => {
   console.log(`Express server listening on port: ${PORT}`)
 })
