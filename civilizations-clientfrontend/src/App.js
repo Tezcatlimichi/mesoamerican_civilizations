db6124d1fa94d2ee1697e9e155e7eefb127686b4
Kathya Castaneda <kathyakathya.kc@gmail.com>
2022-10-24 16:36:54 -0500

    created header , imported axios, useState and useEffect

---
+++ civilizations-clientfrontend/src/App.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react'
+
+import axios from 'axios'
+
+function App() {
+  return (
+    <div>
+      <h1>Mesoamerican Civilizations</h1>
+    </div>
+  )
+}
+
+export default App
