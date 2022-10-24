a76c8d46dbaf985c866ea4504399fd154350c9f8
Kathya Castaneda <kathyakathya.kc@gmail.com>
2022-10-24 10:44:11 -0500

    created civilization mongoose model

--- models/index.js
+++ models/index.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose')
 const countrySchema = require('./country')
+const civilizationSchema = require('./civilization')
 
 const Country = mongoose.model('Country', countrySchema)
+const Civilization = mongoose.model('Civilization', civilizationSchema)
 
 module.exports = {
-  Country
+  Country,
+  Civilization
 }
