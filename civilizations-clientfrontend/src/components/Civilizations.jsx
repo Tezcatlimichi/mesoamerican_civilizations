6222fa3a49fbd7dec605bc50f7885d2510db9fa6
Kathya Castaneda <kathyakathya.kc@gmail.com>
2022-10-25 16:00:57 -0500

    changed civilizations schema to take an array for countries associated with that civilization

---
+++ civilizations-clientfrontend/src/components/Civilizations.jsx
@@ -0,0 +1,40 @@
+
+import {useNavigate} from "react-router-dom"
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
+
+const Civilizations = () => {
+
+  const [civilizations, setCivilizations] = useState([])
+  let {id} =useParams()
+
+  useEffect(() => {
+    const apiCall = async () => {
+      let response = await axios.get('http://localhost:3001/civilizations')
+      setCivilizations(response.data)
+    }
+    apiCall()
+  }, [])
+  
+  let navigate = useNavigate()
+
+  const showCivilization = (civilization) => {
+    navigate(`${civilization.id}`)
+
+  }
+  
+
+  return (
+    <div className="civilization-grid">
+      {
+      civilizations.map((civilization) => (
+        <div className="civilization-card" onClick={() => showCivilization(civilization)} key={civilization.id}>
+          <h3>{civilization.name}</h3>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Civilizations
