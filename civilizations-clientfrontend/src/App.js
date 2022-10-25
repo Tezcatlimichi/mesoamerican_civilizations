57953b81efb2bf69d116438f258bd58bf57218c2
Kathya Castaneda <kathyakathya.kc@gmail.com>
2022-10-25 10:13:55 -0500

    created my components

--- civilizations-clientfrontend/src/App.js
+++ civilizations-clientfrontend/src/App.js
@@ -1,11 +1,25 @@
 import { useState, useEffect } from 'react'
-
 import axios from 'axios'
 
 function App() {
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    const apiCall = async () => {
+      let response = await axios.get('http://localhost:3001/countries')
+      setCountries(response.data)
+    }
+    apiCall()
+  }, [])
+
   return (
-    <div>
+    <div className="App">
       <h1>Mesoamerican Civilizations</h1>
+      {countries.map((country) => (
+        <div>
+          <h2>{country.name}</h2>
+        </div>
+      ))}
     </div>
   )
 }
