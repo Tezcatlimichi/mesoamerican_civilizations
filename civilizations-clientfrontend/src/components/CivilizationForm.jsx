7c90d8a2810afc1989a0b63b320c9c981b270859
Kathya Castaneda <kathyakathya.kc@gmail.com>
2022-10-27 11:28:30 -0500

    created form

--- civilizations-clientfrontend/src/components/CivilizationForm.jsx
+++ civilizations-clientfrontend/src/components/CivilizationForm.jsx
@@ -3,7 +3,14 @@
 const CivilizationForm = () =>{
   return(
     <div>
-
+      <h1>TESTING FORM</h1>
+      <form >
+      <input type="text"  />
+      <input type="text" />
+      <input type="text-area"  />
+      <input type="text"  />
+      <button>Submit</button>
+    </form>
     </div>
   )
 }
