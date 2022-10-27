ed9aafb28eb1b5c3fa3f7fbae36412011c869140
Kathya Castaneda <kathyakathya.kc@gmail.com>
2022-10-27 10:45:07 -0500

    got my selected civilization to render its information

--- civilizations-clientfrontend/src/components/Civilizations.jsx
+++ civilizations-clientfrontend/src/components/Civilizations.jsx
@@ -3,12 +3,22 @@ import axios from 'axios'
 import { useParams } from 'react-router-dom'
 
 const Civilizations = () => {
+ const [civilization, setCivilization] = useState("")
 
   let {id} =useParams()
 
+  useEffect(()=>{
+    const apiCall = async () => {
+      const response = await axios.get(`http://localhost:3001/civilizations/${id}`)
+      setCivilization(response.data)
+    }
+    apiCall()
+  },[])
+
   return (
     <div className="civilization-grid">
-      <h2></h2>
+      <h2>{civilization.name}</h2>
+      <h3>{civilization.description}</h3>
     </div>
   )
 }
